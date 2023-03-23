a509cb4e7ea5814b149c7234574f357deb0c4131
Anette <polosukhina.ai@sibvaleo.com>
2023-03-23 22:03:42 +0700

    правки

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,12 +1,14 @@
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
-
+String name = "Anna";
 
     @Test
     public void testHello(){
-    System.out.println("Hello w");
+        System.out.println("Hello from "+name);
 }
 
 }
